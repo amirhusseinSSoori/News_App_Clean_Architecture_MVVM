f8634236f989d3bdd3976f3e499dd86a175fb547
Soori <soori@hamtasharif.ir>
2021-12-13 01:04:15 -0800

    keep State in navigation component

--- presentation/src/main/java/com/amirhusseinsoori/newsapp/presentation/NewsActivity.kt
+++ presentation/src/main/java/com/amirhusseinsoori/newsapp/presentation/NewsActivity.kt
@@ -9,36 +9,36 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.amirhusseinsoori.newsapp.R
+import com.amirhusseinsoori.newsapp.presentation.util.BottomNavManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NewsActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    private var bottomNavManager: BottomNavManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+        setupNavigationManager()
 
-
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.nav_host_fragment
-        ) as NavHostFragment
-        navController = navHostFragment.navController
-
-        // Setup the bottom navigation view with navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.setupWithNavController(navController)
-
-        // Setup the ActionBar with navController and 3 top level destinations
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.breaking_list, R.id.saved_List,  R.id.search_list)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+    private fun setupNavigationManager() {
+        bottomNavManager?.setupNavController() ?: kotlin.run {
+            bottomNavManager = BottomNavManager(
+                fragmentManager = supportFragmentManager,
+                containerId = R.id.nav_host_fragment,
+                bottomNavigationView = findViewById(R.id.bottomNavigationView)
+            )
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        bottomNavManager?.onSaveInstanceState(outState)
     }
 
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        bottomNavManager?.onRestoreInstanceState(savedInstanceState)
+        setupNavigationManager()
     }
 }
